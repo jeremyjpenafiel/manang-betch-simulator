c73377afd680ee24d58719e276bdce07559cff1b
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-22 15:32:39 +0800

    add game initiator

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class GameInitiator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private new Camera camera;
+    [SerializeField] private Light mainDirectionalLight;
+    [SerializeField] private EventSystem mainEventSystem;
+    [SerializeField] private Canvas loadingCanvas;
+    
+    [SerializeField] private IngredientSystem ingredientSystem;
+    private async void Start()
     {
-        
+        BindObjects();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void BindObjects()
     {
-        
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+        camera = Instantiate(camera);
+        mainDirectionalLight = Instantiate(mainDirectionalLight);
+        mainEventSystem = Instantiate(mainEventSystem);
+        ingredientSystem = Instantiate(ingredientSystem);
+        loadingCanvas = Instantiate(loadingCanvas);
     }
 }
