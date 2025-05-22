94738d881deb84add6b715c84d1ad6f2dcb3298c
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-22 15:42:20 +0800

    Update GameInitiator.cs

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -10,19 +11,29 @@ public class GameInitiator : MonoBehaviour
     [SerializeField] private EventSystem mainEventSystem;
     [SerializeField] private Canvas loadingCanvas;
     
-    [SerializeField] private IngredientSystem ingredientSystem;
+    // [SerializeField] private IngredientSystem ingredientSystem;
     private async void Start()
     {
         BindObjects();
+        // await LoadScene();
     }
 
     private void BindObjects()
     {
-        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
         camera = Instantiate(camera);
         mainDirectionalLight = Instantiate(mainDirectionalLight);
         mainEventSystem = Instantiate(mainEventSystem);
-        ingredientSystem = Instantiate(ingredientSystem);
+        // ingredientSystem = Instantiate(ingredientSystem);
         loadingCanvas = Instantiate(loadingCanvas);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+        
     }
+
+    // private async UniTask LoadScene()
+    // {
+    //     loadingCanvas.gameObject.SetActive(true);
+    //     await UniTask.Delay(3000);
+    //     loadingCanvas.gameObject.SetActive(false);
+    //     SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+    // }
 }
