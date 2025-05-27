e5a621ca2c381e5a5f6fd2d59bf056e39203569b
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-28 01:31:15 +0800

    cleaned initiator

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -29,24 +29,14 @@ public class GameInitiator : MonoBehaviour
 
     private void BindObjects()
     {
-        // camera = Instantiate(camera, Vector3.zero, Quaternion.identity);
-        // camera = Instantiate(camera);
-        // moneyText = camera.GetComponentInChildren<MoneyText>();
-        // moneyText.Initialize();
+
         mainDirectionalLight = Instantiate(mainDirectionalLight, Vector3.zero, Quaternion.identity);
         mainEventSystem = Instantiate(mainEventSystem, Vector3.zero, Quaternion.identity);
-        // ingredientSystem = Instantiate(ingredientSystem);
         loadingCanvas = Instantiate(loadingCanvas);
-        // SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
         cubPrefab = Instantiate(cubPrefab, Vector3.zero, Quaternion.identity);
-        // player at -9757, 15426, 2851
         player = Instantiate(player, new Vector3(-9190, 1033, 2221), Quaternion.identity);
         npcSystem = Instantiate(npcSystem);
         orderCreator = Instantiate(orderCreator);
-        // _npcSpawner = Instantiate(_npcSpawner, new Vector3(2091,85.017395f,-10304), Quaternion.identity);
-        // Instantiate(queuePoint, queuePoint.transform.position, Quaternion.identity);
-
-
     }
 
     // private async UniTask LoadScene()
