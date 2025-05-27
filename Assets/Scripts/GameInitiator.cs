47c9a78f0a65713bc565d43bfe87b1a9e4afdf17
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-27 16:38:53 +0800

    rename camera to gameCamera
    also changed some instantiate

--- Assets/Scripts/GameInitiator.cs
+++ Assets/Scripts/GameInitiator.cs
@@ -4,10 +4,11 @@ using NPCSystem;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class GameInitiator : MonoBehaviour
 {
-    [SerializeField] private GameObject camera;
+    [FormerlySerializedAs("camera")] [SerializeField] private GameObject gameCamera;
     [SerializeField] private Light mainDirectionalLight;
     [SerializeField] private EventSystem mainEventSystem;
     [SerializeField] private Canvas loadingCanvas;
@@ -38,8 +39,8 @@ public class GameInitiator : MonoBehaviour
         cubPrefab = Instantiate(cubPrefab, Vector3.zero, Quaternion.identity);
         // player at -9757, 15426, 2851
         player = Instantiate(player, new Vector3(-9190, 1033, 2221), Quaternion.identity);
-        _npcSpawner = Instantiate(_npcSpawner, new Vector3(2091,85.017395f,-10304), Quaternion.identity);
-        Instantiate(queuePoint, queuePoint.transform.position, Quaternion.identity);
+        // _npcSpawner = Instantiate(_npcSpawner, new Vector3(2091,85.017395f,-10304), Quaternion.identity);
+        // Instantiate(queuePoint, queuePoint.transform.position, Quaternion.identity);
 
 
     }
