2e09c6843ecef244e6ac437c7b7d1ebe42dbab32
Franz Ferrer <fpaferrer@gmail.com>
2025-05-29 07:48:43 +0800

    Connected Day Summary UI to PlayerStats

---
+++ Assets/Scripts/BlackboardUISystem/DaySummaryUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class DaySummaryUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PlayerStatistics playerStatistics;
+
+    [SerializeField] private TMP_Text totalIncomeText;
+    [SerializeField] private TMP_Text successfulTransactionsText;
+    [SerializeField] private TMP_Text missedOrdersText;
+    [SerializeField] private TMP_Text foodThrownText;
+
+    public void DisplaySummary()
+    {
+        totalIncomeText.text = $"Total Income: ₱{playerStatistics.DailyIncome:F2}";
+        successfulTransactionsText.text = $"Successful Transactions: {playerStatistics.Money / 50:F0}"; // Example logic
+        missedOrdersText.text = $"Missed Orders: {playerStatistics.MissedOrders}";
+        foodThrownText.text = $"Food Thrown: {playerStatistics.ThrownFoodTimes}";
+    }
+}
